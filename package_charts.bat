076509c2ceb1b03fee508d7ae5822c5ce6dd9f3d
Jose Turegano <jose.turegano@panel.es>
2023-07-03 14:02:41 +0200

    Errors

--- package_charts.bat
+++ package_charts.bat
@@ -3,8 +3,6 @@ setlocal enabledelayedexpansion
 
 set CHARTS_DIR=charts
 set REPO_URL=https://github.com/zahori-io/zahori-helm-charts
-set GPG_KEY_ID=9002117265A80213EE15FB4889BA70874C7C7892
-set GPG_PASSPHRASE=$SReR6!jrM3fdS
 
 for /D %%I in ("%CHARTS_DIR%\*") do (
     echo Packaging chart: %%~nxI
